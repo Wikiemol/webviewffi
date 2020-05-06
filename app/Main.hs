3777c5cee66545e885514da1c6ae73f8ccc2bfa4
William Oliver <kingweko@gmail.com>
2020-05-06 01:57:05 -0400

    Adding the rest of the ffi functions

--- app/Main.hs
+++ app/Main.hs
@@ -7,10 +7,16 @@ import Foreign.C
 
 main :: IO ()
 main = do 
-    uri <- newCString "https://en.m.wikipedia.org/wiki/Main_Page"
+    uri <- newCString "http://example.com/"
     title <- newCString "My Title"
     w <- webview_create True nullPtr
     webview_set_title w title
     webview_set_size w 500 500 0
     webview_navigate w uri
+
+    testFunName <- newCString "helloWorld"
+    helloWorldContents <- newCString "console.log('Hello world!');"
+    webview_bind w testFunName (\_ _ _ -> webview_eval w helloWorldContents) nullPtr
+    runHelloWorldContents <- newCString "helloWorld();"
+    webview_init w runHelloWorldContents
     webview_run w
--- webviewffi/WebviewFFI.hs
+++ webviewffi/WebviewFFI.hs
@@ -1,10 +1,18 @@
 module WebviewFFI
     ( 
         webview_create,
-        webview_set_size,
-        webview_set_title,
-        webview_navigate,
+        webview_destroy,
         webview_run,
+        webview_terminate,
+        webview_dispatch,
+        webview_get_window,
+        webview_set_title,
+        webview_set_size,
+        webview_navigate,
+        webview_init,
+        webview_eval,
+        webview_bind,
+        webview_return,
         Window,
         Webview
     ) where
@@ -14,9 +22,35 @@ import Foreign.C
 
 data Window = Window;
 data Webview = Webview;
-
 foreign import ccall "ffi_webview_create" webview_create :: Bool -> Ptr Window -> IO (Ptr Webview)
-foreign import ccall "ffi_webview_set_size" webview_set_size :: Ptr Webview -> CInt -> CInt -> CInt -> IO ()
-foreign import ccall "ffi_webview_set_title" webview_set_title :: Ptr Webview -> CString -> IO ()
-foreign import ccall "ffi_webview_navigate" webview_navigate :: Ptr Webview -> CString -> IO ()
+foreign import ccall "ffi_webview_destroy" webview_destroy :: Ptr Webview -> IO ()
 foreign import ccall "ffi_webview_run" webview_run :: Ptr Webview -> IO ()
+foreign import ccall "ffi_webview_terminate" webview_terminate :: Ptr Webview -> IO ()
+foreign import ccall "ffi_webview_dispatch" c_webview_dispatch :: Ptr Webview -> FunPtr (DispatchCallback a) -> Ptr a -> IO ()
+foreign import ccall "ffi_webview_get_window" webview_get_window :: Ptr Webview -> IO (Ptr Window)
+foreign import ccall "ffi_webview_set_title" webview_set_title :: Ptr Webview -> CString -> IO ()
+foreign import ccall "ffi_webview_set_size" webview_set_size :: Ptr Webview -> CInt -> CInt -> CInt -> IO ()
+foreign import ccall "ffi_webview_navigate" webview_navigate :: Ptr Webview -> CString -> IO ()
+foreign import ccall "ffi_webview_init" webview_init :: Ptr Webview -> CString -> IO ()
+foreign import ccall "ffi_webview_eval" webview_eval :: Ptr Webview -> CString -> IO ()
+foreign import ccall "ffi_webview_bind" c_webview_bind :: Ptr Webview -> CString -> FunPtr (BindCallback a) -> Ptr a -> IO ()
+foreign import ccall "ffi_webview_return" webview_return :: Ptr Webview -> CString -> CInt -> CString -> IO ()
+
+
+type DispatchCallback a = Ptr Webview -> Ptr a -> IO ()
+foreign import ccall "wrapper"
+  mkDispatchCallback :: DispatchCallback a -> IO (FunPtr (DispatchCallback a))
+
+webview_dispatch :: Ptr Webview -> (DispatchCallback a) -> Ptr a -> IO ()
+webview_dispatch wv callback args = do 
+    c_callback <- mkDispatchCallback callback
+    c_webview_dispatch wv c_callback args
+
+type BindCallback a = CString -> CString -> Ptr a -> IO ()
+foreign import ccall "wrapper"
+  mkBindCallback :: BindCallback a -> IO (FunPtr (BindCallback a))
+
+webview_bind :: Ptr Webview -> CString -> BindCallback a -> Ptr a -> IO ()
+webview_bind wv name callback args = do 
+    c_callback <- mkBindCallback callback
+    c_webview_bind wv name c_callback args
