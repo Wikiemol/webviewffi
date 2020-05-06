8076ad657bf27af2f9b863cf463be689fcd62d51
William Oliver <kingweko@gmail.com>
2020-05-06 08:59:28 -0400

    Updating ffi to use more canonical Haskell types

--- app/Main.hs
+++ app/Main.hs
@@ -7,16 +7,11 @@ import Foreign.C
 
 main :: IO ()
 main = do 
-    uri <- newCString "http://example.com/"
-    title <- newCString "My Title"
     w <- webview_create True nullPtr
-    webview_set_title w title
+    webview_set_title w "My Title"
     webview_set_size w 500 500 0
-    webview_navigate w uri
+    webview_navigate w "http://example.com/"
 
-    testFunName <- newCString "helloWorld"
-    helloWorldContents <- newCString "console.log('Hello world!');"
-    webview_bind w testFunName (\_ _ _ -> webview_eval w helloWorldContents) nullPtr
-    runHelloWorldContents <- newCString "helloWorld();"
-    webview_init w runHelloWorldContents
+    webview_bind w "helloWorld" (\_ _ _ -> webview_eval w "console.log('Hello world!');") nullPtr
+    webview_init w "helloWorld();"
     webview_run w
